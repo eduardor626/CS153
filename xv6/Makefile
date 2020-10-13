190752412799435d64def040782a42ef874e3070
Eduardo Rocha <eroch007@sledge.cs.ucr.edu>
2020-10-13 15:58:35 -0700

    finishing lab0

--- xv6/Makefile
+++ xv6/Makefile
@@ -181,6 +181,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_testhello\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- xv6/sysproc.c
+++ xv6/sysproc.c
@@ -89,3 +89,10 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_hello(void)
+{
+	cprintf("hello from the kernel\n");
+	return 0;
+}
